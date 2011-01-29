6c5854c693e79f8a169adb5cb1a87a59c231a760
richard <richard@badham.com>
2011-01-29 13:22:12 +0100

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def registration
+    @title = "Registration"
   end
 
   def contact
+    @title = "Contact"
   end
 
 end
